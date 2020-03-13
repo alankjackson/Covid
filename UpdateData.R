ed33de69759ad26af4214b0aee30d973f5e31f84
alankjackson <alan@ajackson.org>
2020-03-12 20:30:39 -0500

    initial commit

---
+++ UpdateData.R
@@ -0,0 +1,32 @@
+#   Read state Health Dept site for Covid update
+
+
+library(tidyverse)
+library(rvest)
+library(httr)
+library(xml2)
+
+options(stringsAsFactors = FALSE)
+
+url <- "https://www.dshs.state.tx.us/news/updates.shtm#coronavirus"
+#   Bad idea, disabling certificate check
+page <- RCurl::getURL("https://www.dshs.state.tx.us/news/updates.shtm#coronavirus", ssl.verifyhost = 0L, ssl.verifypeer = 0L)
+
+mytable <- read_html(page) %>% 
+  html_nodes(xpath='//*[@id="ctl00_ContentPlaceHolder1_uxContent"]/div[2]/table') %>% 
+  html_table()
+
+mytable <- mytable[[1]]
+names(mytable) <- c("County", "Cases")
+mytable <- mytable %>% mutate(Date=lubridate::today())
+
+# Read in the old data
+CovidData <- readRDS("/home/ajackson/Dropbox/Rprojects/Covid/Covid.rds")
+# append the new data
+CovidData <- bind_rows(CovidData, mytable)
+# Save an accumulated file in case of a failure
+saveRDS(CovidData,paste0("/home/ajackson/Dropbox/Rprojects/Covid/",lubridate::today(),"_Covid.rds"))
+# Save the real file for later use
+saveRDS(CovidData,"/home/ajackson/Dropbox/Rprojects/Covid/Covid.rds")
+# Also save to mirror site
+saveRDS(CovidData,"/home/ajackson/Dropbox/mirrors/ajackson/Covid/Covid.rds")
