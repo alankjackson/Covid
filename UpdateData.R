f26a618dabec381f8bee23f267c561a494267895
alankjackson <alan@ajackson.org>
2020-03-19 13:55:59 -0500

    Add code to scrape and save number of tests

--- UpdateData.R
+++ UpdateData.R
@@ -24,6 +24,19 @@ mytable <- mytable %>% mutate(Date=lubridate::today())
 # Get rid of footnotes
 mytable$County <- str_replace(mytable$County, "\\d", "")
 
+
+testing_status <- read_html(page) %>% 
+  html_nodes(xpath='/html/body/form/div[4]/div/div[3]/div[2]/div/div/div[2]/table[1]') %>% 
+  html_table()
+
+testing_status <-   testing_status[[1]][["X2"]] %>% 
+  str_remove_all("\\D") %>% 
+  tibble(x=.) %>% 
+  mutate(y=c("Total", "Public", "Private")) %>% 
+  spread(key=y, value=x)
+
+testing_status <- testing_status %>% mutate(Date=lubridate::today())
+
 # Read in the old data
 CovidData <- readRDS("/home/ajackson/Dropbox/Rprojects/Covid/Covid.rds")
 # append the new data
@@ -34,5 +47,16 @@ saveRDS(CovidData,paste0("/home/ajackson/Dropbox/Rprojects/Covid/",lubridate::to
 saveRDS(CovidData,"/home/ajackson/Dropbox/Rprojects/Covid/Covid.rds")
 # Also save to mirror site
 saveRDS(CovidData,"/home/ajackson/Dropbox/mirrors/ajackson/Covid/Covid.rds")
+
+# Read in the old data
+TestingData <- readRDS("/home/ajackson/Dropbox/Rprojects/Covid/Testing.rds")
+# append the new data
+TestingData <- bind_rows(TestingData, testing_status)
+# Save an accumulated file in case of a failure
+saveRDS(TestingData,paste0("/home/ajackson/Dropbox/Rprojects/Covid/",lubridate::today(),"_Testing.rds"))
+# Save the real file for later use
+saveRDS(TestingData,"/home/ajackson/Dropbox/Rprojects/Covid/Testing.rds")
+# Also save to mirror site
+saveRDS(TestingData,"/home/ajackson/Dropbox/mirrors/ajackson/Covid/Testing.rds")
 
 print(paste("Ending run ---------------------", lubridate::now()))
