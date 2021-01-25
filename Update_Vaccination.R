ff8399ea7170bf2e4a6ef84c4b80a044a7c88682
alankjackson <alan@ajackson.org>
2021-01-25 15:08:26 -0600

    Fix problem with non-numeric fields Clean up cruft

--- Update_Vaccination.R
+++ Update_Vaccination.R
@@ -13,7 +13,9 @@ cat("\n=============== Vaccine updates started =========\n\n")
 
 this_day <- lubridate::today()
 
-this_day <- lubridate::ymd("2021-01-21")
+#############
+# this_day <- lubridate::ymd("2020-12-28")
+#############
 
 path <- "/home/ajackson/Dropbox/Rprojects/CovidTempData/TexasDataXcel/"
 
@@ -32,18 +34,6 @@ writeBin(Vaccine, Vaccine_path)
 
 #  Read file into tibble
 
-namekey <- c(County="County Name", 
-             Region="Public Health Region (PHR)", 
-             Doses_Alloc="Total Doses Allocated", 
-             Doses_Admin="Vaccine Doses Administered", 
-             People_one_dose="People Vaccinated with at least One Dose", 
-             People_fully="People Fully Vaccinated", 
-             Pop_Adult="Population, 16+",
-             Pop_old="Population, 65+", 
-             Pop_Healthcare="Population, Phase 1A Healthcare Workers",
-             Pop_LongTerm="Population, Phase 1A Long-term Care Residents",
-             Pop_1B_Medical="Population, Phase 1B Any Medical Condition")
-
 namekey <- c("County Name"="County", 
              "Public Health Region (PHR)"="Region", 
              "Total Doses Allocated"="Doses_alloc", 
@@ -56,31 +46,10 @@ namekey <- c("County Name"="County",
              "Population, Phase 1A Long-term Care Residents"="Pop_longterm",
              "Population, Phase 1B Any Medical Condition"="Pop_1B_medical")
 
-foo <- readxl::read_excel(Vaccine_path, sheet=2)  
+foo <- readxl::read_excel(Vaccine_path, sheet=2, na=c("", "--"))  
 
 names(foo) <- namekey[names(foo)]
 
-# Fix Federal Long-Term Care Vaccination Program
-
-#  rename(County="County Name", 
-#         Region="Public Health Region (PHR)", 
-#         Doses_Alloc="Total Doses Allocated", 
-#         Doses_Admin="Vaccine Doses Administered", 
-#         People_one_dose="People Vaccinated with at least One Dose", 
-#         People_fully="People Fully Vaccinated", 
-#         Pop_Adult="Population, 16+",
-#         Pop_old="", 
-#         Pop_Healthcare=9) #%>% 
-#
-  #rename(County=1, Region=2, Doses_Distrib=3, Doses_Admin=4, People_one_dose=5, People_fully=6, Pop_Adult=7,
-  #          Pop_old=8, Pop_Healthcare=9) %>% 
-#   filter(County!="County", County!="Total") %>% 
-#  mutate(Date=this_day) %>% 
-#  mutate(County=str_replace_all(County, "[\r\n]" , "")) #%>% 
-#   mutate(County=str_replace(County, "SanAugustine", "San Augustine"))
-# foo$Cases <- as.numeric(foo$Cases)
-# foo$Deaths <- as.numeric(foo$Deaths)
-# 
 head(foo)
 tail(foo)
 
