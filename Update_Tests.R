c3a3641da2fc47038c1b4bc6297983ab9542d0b7
alankjackson <alan@ajackson.org>
2022-04-20 19:02:00 -0500

    minor updates

--- Update_Tests.R
+++ Update_Tests.R
@@ -32,46 +32,107 @@ Tests_path <- paste0(path, "Tests_by_County_", this_day,".xlsx")
 writeBin(Tests, Tests_path)
 
 #  Read file into tibble
+# 
+# foo <- readxl::read_excel(Tests_path)
+# 
+# my_colnames <- lubridate::ymd("2020-09-13")+0:(ncol(foo)-2)
+# my_colnames <- my_colnames %>% 
+#   format('%Y-%m-%d')
+# my_colnames <- c("County", my_colnames)
+# my_columns <- c(1:ncol(foo))
+# 
+# foo <- foo %>% 
+#   rename_at(my_columns,~ my_colnames)
+# 
+# foo <- foo[2:257,] # delete bad rows
+# #foo <- foo[-16] # delete bad column
+# 
+# foo <- foo %>% pivot_longer(-County, names_to="Date", values_to="Tests")
+# 
+# foo <- foo %>% mutate(Tests=as.numeric(Tests))
+# foo <- foo %>% mutate(Date=lubridate::as_date(Date))
+# 
+# total_tests <- last(foo$Tests[foo$County=="Total"])
 
-foo <- readxl::read_excel(Tests_path)
+#   2020
 
-# No data for May 5
+df <- readxl::read_excel(Tests_path,
+                         sheet="Total Tests 2020",
+                         range=readxl::cell_rows(3:258),
+                         col_names=FALSE)
 
-#my_colnames <- c(
-#  lubridate::ymd("2020-04-21")+0:13,  
-#  lubridate::ymd("2020-05-06")+0:(ncol(foo)-17))
-#
-#my_colnames <- my_colnames %>% 
-#  format('%Y-%m-%d')
-#my_colnames <- c("County", my_colnames)
-#
-#my_columns <- c(1:15, 17:ncol(foo))
-
-my_colnames <- lubridate::ymd("2020-09-13")+0:(ncol(foo)-2)
+my_colnames <- lubridate::ymd("2020-09-13")+0:(ncol(df)-2)
 my_colnames <- my_colnames %>% 
   format('%Y-%m-%d')
 my_colnames <- c("County", my_colnames)
-my_columns <- c(1:ncol(foo))
+my_columns <- c(1:ncol(df))
 
-foo <- foo %>% 
+df <- df %>% 
   rename_at(my_columns,~ my_colnames)
 
-foo <- foo[2:257,] # delete bad rows
-#foo <- foo[-16] # delete bad column
+df <- df %>% pivot_longer(-County, names_to="Date", values_to="Tests")
 
-foo <- foo %>% pivot_longer(-County, names_to="Date", values_to="Tests")
+df <- df %>% mutate(Tests=as.numeric(Tests))
+df <- df %>% mutate(Date=lubridate::as_date(Date))
 
-foo <- foo %>% mutate(Tests=as.numeric(Tests))
-foo <- foo %>% mutate(Date=lubridate::as_date(Date))
+#   2021
 
-total_tests <- last(foo$Tests[foo$County=="Total"])
+df2 <- readxl::read_excel(Tests_path,
+                         sheet="Total Tests 2021",
+                         range=readxl::cell_rows(3:258),
+                         col_names=FALSE)
+
+my_colnames <- lubridate::ymd("2021-01-01")+0:(ncol(df2)-2)
+my_colnames <- my_colnames %>% 
+  format('%Y-%m-%d')
+my_colnames <- c("County", my_colnames)
+my_columns <- c(1:ncol(df2))
+
+df2 <- df2 %>% 
+  rename_at(my_columns,~ my_colnames)
+
+df2 <- df2 %>% pivot_longer(-County, names_to="Date", values_to="Tests")
+
+df2 <- df2 %>% mutate(Tests=as.numeric(Tests))
+df2 <- df2 %>% mutate(Date=lubridate::as_date(Date))
+
+#   Merge the two after some cleanup
+
+df <- rbind(df, df2)
+
+#   2022
+
+df2 <- readxl::read_excel(Tests_path,
+                         sheet="Total Tests 2022",
+                         range=readxl::cell_rows(3:258),
+                         col_names=FALSE)
+
+my_colnames <- lubridate::ymd("2022-01-01")+0:(ncol(df2)-2)
+my_colnames <- my_colnames %>% 
+  format('%Y-%m-%d')
+my_colnames <- c("County", my_colnames)
+my_columns <- c(1:ncol(df2))
+
+df2 <- df2 %>% 
+  rename_at(my_columns,~ my_colnames)
+
+df2 <- df2 %>% pivot_longer(-County, names_to="Date", values_to="Tests")
+
+df2 <- df2 %>% mutate(Tests=as.numeric(Tests))
+df2 <- df2 %>% mutate(Date=lubridate::as_date(Date))
+
+#   Merge the two after some cleanup
+
+df <- rbind(df, df2)
+
+total_tests <- last(df$Tests[df$County=="Total"])
 
 testing_status <- tribble(
   ~Total, ~Public, ~Private,
   total_tests, NA, NA
 )   
 
-tail(foo)
+tail(df)
 
 testing_status <- testing_status %>% 
   mutate(Date=lubridate::today()) %>% 
@@ -90,7 +151,7 @@ if(last(TestingData$Total)==testing_status$Total) { # no change. Abort
 Legacy_data <- readRDS("/home/ajackson/Dropbox/Rprojects/CovidTempData/DailyBackups/2020-09-13_County_Testing.rds")
 Legacy_data <- Legacy_data %>% mutate(Date=lubridate::as_date(Date))
 
-foo <- rbind(Legacy_data, foo)
+df <- rbind(Legacy_data, df)
 
 
 ################   Testing data
@@ -104,11 +165,11 @@ saveRDS(TestingData,"/home/ajackson/Dropbox/Rprojects/Covid/Testing.rds")
 saveRDS(TestingData,"/home/ajackson/Dropbox/mirrors/ajackson/Covid/Testing.rds")
 
 # Save county level file as well
-saveRDS(foo,paste0("/home/ajackson/Dropbox/Rprojects/CovidTempData/DailyBackups/",this_day,"_County_Testing.rds"))
-saveRDS(foo,paste0("/home/ajackson/Dropbox/Rprojects/Covid/Today_Data/Today_County_Testing.rds"))
-saveRDS(foo,paste0("/home/ajackson/Dropbox/mirrors/ajackson/Covid/Today_County_Testing.rds"))
+saveRDS(df,paste0("/home/ajackson/Dropbox/Rprojects/CovidTempData/DailyBackups/",this_day,"_County_Testing.rds"))
+saveRDS(df,paste0("/home/ajackson/Dropbox/Rprojects/Covid/Today_Data/Today_County_Testing.rds"))
+saveRDS(df,paste0("/home/ajackson/Dropbox/mirrors/ajackson/Covid/Today_County_Testing.rds"))
 # Save the real file for later use
-saveRDS(foo,"/home/ajackson/Dropbox/Rprojects/Covid/County_Testing.rds")
+saveRDS(df,"/home/ajackson/Dropbox/Rprojects/Covid/County_Testing.rds")
 
 cat("\n\n=============== Testing updates finished =========\n\n")
 print(lubridate::now())
